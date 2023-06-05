df1cdb54ec18af0844166790fc243cb9d2bc739e
adarshnaman <58041512+adarshnaman@users.noreply.github.com>
2023-06-05 09:51:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit
+import pandas
 
-streamlit.title("Hello")
+streamlit.title("My mom's new healthy dinner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('F
